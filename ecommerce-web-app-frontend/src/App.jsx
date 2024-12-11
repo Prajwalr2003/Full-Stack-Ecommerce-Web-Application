3b4fc900b39cca3eb6a87647f3cc4de01424a652
Prajwalr2003 <prajwalrangari513@gmail.com>
2024-12-11 16:59:43 +0530

    changes to frontend files

--- ecommerce-web-app-frontend/src/App.jsx
+++ ecommerce-web-app-frontend/src/App.jsx
@@ -2,7 +2,6 @@ import Footer from "./components/FooterBar";
 import Header from "./components/Header";
 import Container from "./components/Container";
 import { Outlet } from "react-router-dom";
-import Home from "./pages/Home";
 
 function App() {
   return (
